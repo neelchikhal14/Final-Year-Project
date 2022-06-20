fc006b6939e5f40cdbf096f88cd5432a47328a01
neelchikhal14 <neelchikhal14@gmail.com>
2022-06-20 22:56:18 +0100

    tailwind setup complete

--- exercisely-frontend/src/App.js
+++ exercisely-frontend/src/App.js
@@ -1,26 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-import './App.css';
-import ExerciseHandlerTypeOne from './components/ExerciseHandlerTypeOne';
-import ExerciseHandlerTypeTwo from './components/ExerciseHandlerTypeTwo';
 const App = () => {
-  const [displayTypeOne, setDisplayTypeOne] = useState(false);
-  const [displayTypeTwo, setDisplayTypeTwo] = useState(false);
-  const [exercise] = useState('right_prone_knee_flexon');
-  useEffect(() => {
-    if (exercise === 'rightTreePose') {
-      setDisplayTypeOne(true);
-      setDisplayTypeTwo(false);
-    } else if (exercise === 'right_prone_knee_flexon') {
-      setDisplayTypeOne(false);
-      setDisplayTypeTwo(true);
-    }
-  }, [exercise]);
-
   return (
     <div>
-      {displayTypeOne && <ExerciseHandlerTypeOne exercise={exercise} />}
-      {displayTypeTwo && <ExerciseHandlerTypeTwo exercise={exercise} />}
+      <h1 className='text-base-green'>Hello World</h1>
     </div>
   );
 };
