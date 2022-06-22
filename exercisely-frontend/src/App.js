da393e5425fb43fb751837a0d98255f82182e734
neelchikhal14 <neelchikhal14@gmail.com>
2022-06-22 13:05:53 +0100

    Designed basic header container without logic.Removed Tailwind

--- exercisely-frontend/src/App.js
+++ exercisely-frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-base-green'>Hello World</h1>
-    </div>
+    <Router>
+      <Header />
+    </Router>
   );
 };
 
