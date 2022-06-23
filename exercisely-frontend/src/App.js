36c83aa2c356be982e2d30fbf43e6de2284b4d0f
neelchikhal14 <neelchikhal14@gmail.com>
2022-06-23 21:43:43 +0100

    Home Page is complete.Login Page, Footer Boiler plate ready

--- exercisely-frontend/src/App.js
+++ exercisely-frontend/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Footer from './components/Footer';
 import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
+import Login from './screens/Login';
 
 const App = () => {
   return (
     <Router>
       <Header />
+      <main className='py-3'>
+        <Route path='/login' component={Login} />
+        <Route path='/' component={HomeScreen} exact />
+      </main>
+      <Footer />
     </Router>
   );
 };
